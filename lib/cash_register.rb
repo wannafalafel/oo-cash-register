2eafc0f26072268bf3d5d9e64329b956266785f5
Timothy Miller <mr.tim.miller123@gmail.com>
2016-11-04 18:12:15 -0600

    Working on it

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
 
+require 'pry'
+
+class CashRegister
+  attr_accessor :total, :discount
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+      @items << title
+      quantity > 1 ? @total += price * quantity : @total += price
+    end
+
+    def apply_discount
+      
+    end
+
+end
